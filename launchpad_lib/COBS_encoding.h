8c49507caab236d931c200beaf786150092f8791
keithwm2165 <keithwm2165@gmail.com>
2019-07-31 09:51:48 -0400

    Changes to handle the new restructured mainhsk

--- launchpad_lib/COBS_encoding.h
+++ launchpad_lib/COBS_encoding.h
@@ -11,7 +11,7 @@
 
 #include "Arduino.h"
 
-#define PACKETMARKER 218    // Globalize packet marker
+#define PACKETMARKER 218	// Globalize packet marker
 
 
 /// \brief A Consistent Overhead Byte Stuffing (COBS) Encoder.
@@ -32,109 +32,109 @@
 class COBS
 {
 public:
-/// \brief Encode a byte buffer with the COBS encoder.
-/// \param buffer A pointer to the unencoded buffer to encode.
-/// \param size  The number of bytes in the \p buffer.
-/// \param encodedBuffer The buffer for the encoded bytes.
-/// \returns The number of bytes written to the \p encodedBuffer.
-/// \warning The encodedBuffer must have at least getEncodedBufferSize() allocated.
-static size_t encode(const uint8_t* buffer,
-                     size_t size,
-                     uint8_t* encodedBuffer)
-{
-	size_t read_index  = 0;
-	size_t write_index = 1;
-	size_t code_index  = 0;
-	uint8_t code       = 1;
+    /// \brief Encode a byte buffer with the COBS encoder.
+    /// \param buffer A pointer to the unencoded buffer to encode.
+    /// \param size  The number of bytes in the \p buffer.
+    /// \param encodedBuffer The buffer for the encoded bytes.
+    /// \returns The number of bytes written to the \p encodedBuffer.
+    /// \warning The encodedBuffer must have at least getEncodedBufferSize() allocated.
+    static size_t encode(const uint8_t* buffer,
+                         size_t size,
+                         uint8_t* encodedBuffer)
+    {
+        size_t read_index  = 0;
+        size_t write_index = 1;
+        size_t code_index  = 0;
+        uint8_t code       = 1;
 
-	while (read_index < size)
-	{
-		/* If the byte is actually the PACKETMARKER, set the last overhead byte/
-		   packetMarker to be the # of bytes away the next PACKETMARKER is */
-		if (buffer[read_index] == PACKETMARKER)
-		{
-			encodedBuffer[code_index] = (code + PACKETMARKER) & 0xFF;
-			code = 1;
-			code_index = write_index++;
-			read_index++;
-		}
-		/* If the byte is not the PACKETMARKER, set the encoded buffer byte to
-		         the current read byte.  */
-		else
-		{
-			encodedBuffer[write_index++] = buffer[read_index++];
-			code++;
+        while (read_index < size)
+        {
+            /* If the byte is actually the PACKETMARKER, set the last overhead byte/
+               packetMarker to be the # of bytes away the next PACKETMARKER is */
+            if (buffer[read_index] == PACKETMARKER)
+            {
+                encodedBuffer[code_index] = (code + PACKETMARKER) & 0xFF;
+                code = 1;
+                code_index = write_index++;
+                read_index++;
+            }
+            /* If the byte is not the PACKETMARKER, set the encoded buffer byte to
+    			     the current read byte.  */
+            else
+            {
+                encodedBuffer[write_index++] = buffer[read_index++];
+                code++;
 
-			if (code == 0xFF)
-			{
-				encodedBuffer[code_index] = code;
-				code = 1;
-				code_index = write_index++;
-			}
-		}
-	}
+                if (code == 0xFF)
+                {
+                    encodedBuffer[code_index] = code;
+                    code = 1;
+                    code_index = write_index++;
+                }
+            }
+        }
 
-	encodedBuffer[code_index] = (code + PACKETMARKER) & 0xFF;
-	/* NOTE!!!!
-	    --We need an additional packetMarker here so that the message sent contains
-	      an ending packetMarker
-	 */
-	encodedBuffer[write_index++] = PACKETMARKER;
+        encodedBuffer[code_index] = (code + PACKETMARKER) & 0xFF;
+        /* NOTE!!!!
+        	--We need an additional packetMarker here so that the message sent contains
+        	  an ending packetMarker
+        	*/
+        encodedBuffer[write_index++] = PACKETMARKER;
 
-	return write_index;
-}
+        return write_index;
+    }
 
 
-/// \brief Decode a COBS-encoded buffer.
-/// \param encodedBuffer A pointer to the \p encodedBuffer to decode.
-/// \param size The number of bytes in the \p encodedBuffer.
-/// \param decodedBuffer The target buffer for the decoded bytes.
-/// \returns The number of bytes written to the \p decodedBuffer.
-/// \warning decodedBuffer must have a minimum capacity of size.
-static size_t decode(const uint8_t* encodedBuffer,
-                     size_t size,
-                     uint8_t* decodedBuffer)
-{
-	if (size == 0) return 0;
+    /// \brief Decode a COBS-encoded buffer.
+    /// \param encodedBuffer A pointer to the \p encodedBuffer to decode.
+    /// \param size The number of bytes in the \p encodedBuffer.
+    /// \param decodedBuffer The target buffer for the decoded bytes.
+    /// \returns The number of bytes written to the \p decodedBuffer.
+    /// \warning decodedBuffer must have a minimum capacity of size.
+    static size_t decode(const uint8_t* encodedBuffer,
+                         size_t size,
+                         uint8_t* decodedBuffer)
+    {
+        if (size == 0) return 0;
 
-	size_t read_index  = 0;
-	size_t write_index = 0;
-	uint8_t code       = 0;
-	uint8_t i          = 0;
+        size_t read_index  = 0;
+        size_t write_index = 0;
+        uint8_t code       = 0;
+        uint8_t i          = 0;
 
-	while (read_index < size)
-	{
-		/* the first code byte is the # of bytes we'll have to read until the
-		       next packetMarker is hit */
-		code = (encodedBuffer[read_index] - PACKETMARKER) & 0xFF;
+        while (read_index < size)
+        {
+            /* the first code byte is the # of bytes we'll have to read until the
+      			   next packetMarker is hit */
+            code = (encodedBuffer[read_index] - PACKETMARKER) & 0xFF;
 
-		if (read_index + code > size && code != 1)
-		{
-			return 0;
-		}
+            if (read_index + code > size && code != 1)
+            {
+                return 0;
+            }
 
-		read_index++;
+            read_index++;
 
-		for (i = 1; i < code; i++)
-		{
-			decodedBuffer[write_index++] = encodedBuffer[read_index++];
-		}
+            for (i = 1; i < code; i++)
+            {
+                decodedBuffer[write_index++] = encodedBuffer[read_index++];
+            }
 
-		if (code != 0xFF && read_index != size)
-		{
-			decodedBuffer[write_index++] = PACKETMARKER;
-		}
-	}
+            if (code != 0xFF && read_index != size)
+            {
+                decodedBuffer[write_index++] = PACKETMARKER;
+            }
+        }
 
-	return write_index;
-}
+        return write_index;
+    }
 
-/// \brief Get the maximum encoded buffer size needed for a given unencoded buffer size.
-/// \param unencodedBufferSize The size of the buffer to be encoded.
-/// \returns the maximum size of the required encoded buffer.
-static size_t getEncodedBufferSize(size_t unencodedBufferSize)
-{
-	return unencodedBufferSize + unencodedBufferSize / 254 + 2;
-}
+    /// \brief Get the maximum encoded buffer size needed for a given unencoded buffer size.
+    /// \param unencodedBufferSize The size of the buffer to be encoded.
+    /// \returns the maximum size of the required encoded buffer.
+    static size_t getEncodedBufferSize(size_t unencodedBufferSize)
+    {
+        return unencodedBufferSize + unencodedBufferSize / 254 + 2;
+    }
 
 };
--- launchpad_lib/CommandResponse.h
+++ launchpad_lib/CommandResponse.h
@@ -1,7 +1,7 @@
 /*
  * CommandResponse.h
- *
- * Declares a set of functions to act as responses to received commands and
+ * 
+ * Declares a set of functions to act as responses to received commands and 
  * error protocols
  *
  */
@@ -13,25 +13,27 @@
 #include <Arduino.h>
 
 /* If these commands are received */
-void whatToDoIfPingPong(housekeeping_hdr_t * hdr_out);
+void whatToDoIfPingPong(uint8_t * data);
 
-void whatToDoIfISR(housekeeping_hdr_t * hdr_out);
+void whatToDoIfISR(uint8_t * data);
 
 void whatToDoIfMap(housekeeping_hdr_t * hdr_out, uint8_t (&downDevs)[7][254], uint8_t numDev);
 
 void whatToDoIfSetPriority(housekeeping_prio_t * hdr_prio, housekeeping_hdr_t * hdr_out, uint8_t * comPriorList);
+														
+int whatToDoIfHeaterControl(uint8_t * data, uint8_t len);
 
-void whatToDoIfHeaterControl(housekeeping_hdr_t * hdr_in, housekeeping_hdr_t * hdr_out, Stream & stream);
+int whatToDoIfTestHeaterControl(uint8_t* data, uint8_t len);
 
-void whatToDoIfTestMode(uint16_t * numTestPackets_p, housekeeping_hdr_t * hdr_out);
+//void whatToDoIfTestMode(uint16_t * numTestPackets_p, uint8_t * data);
 
 bool checkThatPriority(housekeeping_hdr_t * hdr_in, housekeeping_hdr_t * hdr_out, uint8_t numSends);
-
+												  
 /* If the criteria for these errors is fulfilled  */
 void error_badDest(housekeeping_hdr_t * hdr_in, housekeeping_hdr_t * hdr_out, housekeeping_err_t * hdr_err);
 void error_badCommand(housekeeping_hdr_t * hdr_in, housekeeping_hdr_t * hdr_out, housekeeping_err_t * hdr_err);
 void error_badLength(housekeeping_hdr_t * hdr_in, housekeeping_hdr_t * hdr_out, housekeeping_err_t * hdr_err);
-void error_badArgs(housekeeping_hdr_t * hdr_in, housekeeping_hdr_t * hdr_out, housekeeping_err_t * hdr_err);
+void error_badArgs(housekeeping_hdr_t * hdr_in, housekeeping_hdr_t * hdr_out, housekeeping_err_t * hdr_err);	
 
-/* Matches on-board data with outgoing packets */
+/* Matches on-board data with outgoing packets */                          				
 void matchData(housekeeping_hdr_t * hdr_out);
