6e718fa1743339f060fddf37fd6c6b74d1f78a12
keithwm2165 <keithwm2165@gmail.com>
2019-07-31 09:55:49 -0400

    Forgot to add the iprotocol in this directory

--- src/iProtocol.h
+++ src/iProtocol.h
@@ -15,10 +15,10 @@
 #include <stdint.h>
 
 /* Standard error types */
-#define EBADDEST        -1  // Packet received for an unknown destination
-#define EBADCOMMAND     -2  // Packet contained an unknown command
-#define EBADLEN         -3  // Packet was the wrong length for the command
-#define EBADARGS        -4  // Packet's command arguments (packets of data)
+#define EBADDEST 		-1	// Packet received for an unknown destination
+#define EBADCOMMAND		-2  // Packet contained an unknown command
+#define EBADLEN			-3	// Packet was the wrong length for the command
+#define EBADARGS		-4	// Packet's command arguments (packets of data)
 
 /*******************************************************************************
 * Typedef enums
@@ -26,14 +26,14 @@
 /* Src/dst definitions. 255 is illegal for source. */
 typedef enum housekeeping_id
 {
-	eSFC = 252,  // Science flight computer
-	eMainHsk = 1, // Main housekeeping board
-	eMagnetHsk = 2, // Magnet housekeeping board
-	eDCTHsk = 3, // DCT housekeeping board
-	eDCTTemp1 = 4, // DCT temperature sensor housekeeping board #1
-	eDCTTemp2 = 5, // DCT temperature sensor housekeeping board #2
+	eSFC = 252,			   // Science flight computer
+	eMainHsk = 1,		   // Main housekeeping board
+	eMagnetHsk = 2,		 // Magnet housekeeping board
+	eDCTHsk = 3,		   // DCT housekeeping board
+	eDCTTemp1 = 4,	   // DCT temperature sensor housekeeping board #1
+	eDCTTemp2 = 5,		 // DCT temperature sensor housekeeping board #2
 	// etc...
-	eBroadcast = 255 // Illegal as a source, but dst=255 means everyone responds
+	eBroadcast = 255	 // Illegal as a source, but dst=255 means everyone responds
 } housekeeping_id;
 
 /* Command definitions */
@@ -45,7 +45,7 @@ typedef enum housekeeping_cmd
 	eIntSensorRead = 2,
 	eMapDevices = 3,
 	eHeaterControl = 4,
-	//2-249 are board-specific
+	eTestHeaterControl = 5,
 	eTestMode = 249,
 	eSendLowPriority = 250,
 	eSendMedPriority = 251,
@@ -72,43 +72,51 @@ typedef enum housekeeping_prio_type
 *******************************************************************************/
 typedef struct housekeeping_hdr_t
 {
-	uint8_t dst; // Destination of packet
-	uint8_t src; // Source of packet
-	uint8_t cmd; // Command (or response) type
-	uint8_t len; // Bytes to follow - 1
+	uint8_t dst; 	// Destination of packet
+	uint8_t src;	// Source of packet
+	uint8_t cmd; 	// Command (or response) type
+	uint8_t len; 	// Bytes to follow - 1
 } housekeeping_hdr_t;
 
 
 typedef struct housekeeping_err_t
 {
-	uint8_t src; // Source of the original packet that caused the error
-	uint8_t dst; // Destination of the original packet that caused the error
-	uint8_t cmd; // Command which caused the error
-	uint8_t error; // Error that occured
+	uint8_t src;	// Source of the original packet that caused the error
+	uint8_t dst;	// Destination of the original packet that caused the error
+	uint8_t cmd;	// Command which caused the error
+	uint8_t error;	// Error that occured
 } housekeeping_err_t;
 
 typedef struct housekeeping_prio_t
 {
-	uint8_t command; // The command we are setting the priority of
-	uint8_t prio_type; // The priority level we are setting it to
+	uint8_t command;	// The command we are setting the priority of
+	uint8_t prio_type;	// The priority level we are setting it to
 } housekeeping_prio_t;
 
 
 /*******************************************************************************
 * Functions
 *******************************************************************************/
-/* Fills the last byte of outgoing packet *p with its checksum */
-void fillChecksum(uint8_t* p);
 
-/* Sums packet to make sure its sum is 0 */
+/* Computes the checksum of a message
+ * Parameters are the range of an array, lowest value is closed, highest is open*/
+//uint8_t computeMySum(const uint8_t * first, const uint8_t * last);
+
+/* Returns true if two checksum values are the same */
+//bool checkMySum(uint8_t & checkI, const uint8_t & checkO);
+
+void fillChecksum(uint8_t* p);
 bool verifyChecksum(uint8_t * p);
+
 
 /* Find address in an array of addresses */
 uint8_t * findMe(uint8_t * first, uint8_t * last, uint8_t address);
+
+
 /*******************************************************************************
 * Defines - global variables
 *******************************************************************************/
-extern housekeeping_hdr_t * hdr_in;     // pointer for loc of incoming data header
-extern housekeeping_hdr_t * hdr_out;    // pointer for loc of outgoing header
-extern housekeeping_err_t * hdr_err;    // pointer for loc of error header
-extern housekeeping_prio_t * hdr_prio;  // pointer for loc of priority header
+extern housekeeping_hdr_t * hdr_in;		// pointer for loc of incoming data header
+extern housekeeping_hdr_t * hdr_out;	// pointer for loc of outgoing header
+extern housekeeping_err_t * hdr_err;	// pointer for loc of error header
+extern housekeeping_prio_t * hdr_prio;	// pointer for loc of priority header
